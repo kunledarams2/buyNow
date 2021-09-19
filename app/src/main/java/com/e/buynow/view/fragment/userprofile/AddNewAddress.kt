eb254e48dbece29f30305bd60a7cf10c985aa5c6
kunledarams2 <Adekunle@1>
2021-09-19 17:55:23 +0100

    user_profile phase 3

--- app/src/main/java/com/e/buynow/view/fragment/userprofile/AddNewAddress.kt
+++ app/src/main/java/com/e/buynow/view/fragment/userprofile/AddNewAddress.kt
@@ -5,11 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.e.buynow.R
+import com.e.buynow.databinding.FragmentAddNewAddressBinding
+import com.e.buynow.databinding.FragmentSaveAddressBinding
+import com.e.buynow.view.fragment.BaseFragment
+import kotlinx.android.synthetic.main.content_home.*
 
 
-class AddNewAddress : Fragment() {
+class AddNewAddress : BaseFragment() {
 
+
+    private var _binding: FragmentAddNewAddressBinding? = null
+    private val binding get() = _binding!!
+    override fun initialiseWidgets() {
+        TODO("Not yet implemented")
+        listener.i
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +35,18 @@ class AddNewAddress : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_new_address, container, false)
+//        return inflater.inflate(R.layout.fragment_add_new_address, container, false)
+        _binding = FragmentAddNewAddressBinding.inflate(inflater,container,false)
+        val view = binding.root
+        setContentView()
+        return view
     }
+    private fun setContentView(){
+
+        binding.createBtn.setOnClickListener {  }
+        binding.backBtn.setOnClickListener { findNavController().navigate(R.id.action_addNewAddress_to_saveAddress) }
+    }
+
 
     companion object {
 
--- app/src/main/java/com/e/buynow/view/fragment/userprofile/SaveAddress.kt
+++ app/src/main/java/com/e/buynow/view/fragment/userprofile/SaveAddress.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.e.buynow.R
-
+import com.e.buynow.databinding.FragmentSaveAddressBinding
 
 
 class SaveAddress : Fragment() {
 
+    private var _binding: FragmentSaveAddressBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +27,16 @@ class SaveAddress : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_save_address, container, false)
+//        return inflater.inflate(R.layout.fragment_save_address, container, false)
+        _binding = FragmentSaveAddressBinding.inflate(inflater,container,false)
+        val view = binding.root
+        setContentView(view)
+        return view
+    }
+
+    private fun setContentView(view: View){
+        binding.backBtn.setOnClickListener { findNavController().navigate(R.id.action_saveAddress_to_profileHomePage) }
+        binding.addNewAddress.setOnClickListener { findNavController().navigate(R.id.action_saveAddress_to_addNewAddress) }
     }
 
     companion object {
